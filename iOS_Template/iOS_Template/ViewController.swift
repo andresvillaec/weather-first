a5858fbca9df0209be6594940859778162a7f242
Sebastian Guerrero <msebgf@yahoo.com>
2019-01-18 14:55:01 -0500

    initial commit

---
+++ iOS_Template/iOS_Template/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_Template
+//
+//  Created by Sebastian Guerrero F on 1/18/19.
+//  Copyright © 2019 DEVSU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
