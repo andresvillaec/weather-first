f1e284e4ca408abec9f880ab3e039640f572d492
Sebastian Guerrero <msebgf@yahoo.com>
2019-01-21 11:53:18 -0500

    enable testable

---
+++ iOS_Template/iOS_Tests/Model/ChoiceTests.swift
@@ -0,0 +1,23 @@
+//
+//  ChoiceTests.swift
+//  iOS_Tests
+//
+//  Created by Sebastian Guerrero F on 1/21/19.
+//  Copyright © 2019 DEVSU. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+class ChoiceTests: QuickSpec {
+  
+  override func spec() {
+    super.spec()
+    
+    describe("should have a name") {
+      
+    }
+    
+  }
+  
+}
