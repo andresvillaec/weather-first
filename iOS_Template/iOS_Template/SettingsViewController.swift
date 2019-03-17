7137577edc6c34a7f814a9c2c61199d4fc958893
Andres Villavicencio <villadosa@hotmail.com>
2019-03-16 22:34:54 -0500

    Added storyboards

---
+++ iOS_Template/iOS_Template/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  iOS_Template
+//
+//  Created by Andres Villavicencio on 3/16/19.
+//  Copyright © 2019 DEVSU. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
